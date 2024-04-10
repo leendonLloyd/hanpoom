f889ad6b43c220ee7b7a3d7b94df30b17de6954a
leendonLloyd <lloyd.dimamay@gmail.com>
2024-04-10 20:56:50 +0800

    chore: improved dto validation

--- src/order/dto/get-picking-slips.dto.ts
+++ src/order/dto/get-picking-slips.dto.ts
@@ -1,14 +1,26 @@
-import { Transform, Type } from 'class-transformer';
-import { IsOptional } from 'class-validator';
-import { PaginateDto } from '../../common/dto/paginate.dto';
-import { PickingSlipStatusEnum } from '../enum/picking-slips.enum';
+import { BadRequestException } from "@nestjs/common";
+import { Transform, Type } from "class-transformer";
+import { IsOptional } from "class-validator";
+import { PaginateDto } from "../../common/dto/paginate.dto";
+import { PickingSlipStatusEnum } from "../enum/picking-slips.enum";
 
 export class GetPickingSlipsDto extends PaginateDto {
   @IsOptional()
   @Type(() => String)
   @Transform((value) => {
     if (value != null) {
-      return value.value.toString().split(',');
+      const acceptedValues = Object.values(PickingSlipStatusEnum);
+      const values = value.value.toString().split(",");
+
+      for (const val of values) {
+        if (!acceptedValues.includes(val)) {
+          throw new BadRequestException([
+            { message: `Invalid enum value for ${value.key}: ${val}`, enums: acceptedValues.join(", ") },
+          ]);
+        }
+      }
+
+      return values;
     }
   })
   status?: PickingSlipStatusEnum[];
@@ -17,7 +29,17 @@ export class GetPickingSlipsDto extends PaginateDto {
   @Type(() => String)
   @Transform((value) => {
     if (value != null) {
-      return value.value.toString() === 'true';
+      console.log('~', value);
+      
+      const values = value.value.toString().split(",");
+
+      for (const val of values) {
+        if (!["true", "false"].includes(val)) {
+          throw new BadRequestException([{ message: `Invalid boolean value for ${value.key}: ${val}` }]);
+        }
+      }
+
+      return values;
     }
   })
   isPreOrder?: boolean = true;
